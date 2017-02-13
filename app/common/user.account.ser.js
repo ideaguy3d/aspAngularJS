e409c8e1175ab7b38512de8f343869bd6c335617
Julius Hernandez Alvarado <JHernandezAlvarado@sjcoe.net>
2017-02-13 14:39:58 -0800

    quick fix

--- app/common/user.account.ser.js
+++ app/common/user.account.ser.js
@@ -16,7 +16,7 @@
 
         return {
             // jUserAccount.register.user()
-            register: $resource(jAppSettings.serverPath + 'api/Account/Register', null,
+            register: $resource(jAppSettings.cloudPath + 'api/Account/Register', null,
                 {
                     'registerUser': {
                         method: 'POST'
@@ -30,7 +30,7 @@
                         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
                         transformRequest: function (data, headersGetter) {
                             var str = [];
-                            console.log(jAppSettings.serverPath + 'Token');
+                            console.log(jAppSettings.cloudPath + 'Token');
                             console.log(data);
                             for (var d in data) {
                                 str.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
